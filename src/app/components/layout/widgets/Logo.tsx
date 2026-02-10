04f99d73004a76604dec48e742b189e08e92c2bb
Griffin250 <griffinugofficial@gmail.com>
2026-02-10 21:14:25 +0100

    Fixed missing logo import issue!

--- src/app/components/layout/widgets/Logo.tsx
+++ src/app/components/layout/widgets/Logo.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import LogoImage from "../../../../assets/IT_Perfect_Logo.png";
 
 interface LogoProps {
   className?: string;
@@ -9,12 +10,11 @@ export default function Logo({ className }: LogoProps) {
   return (
     <Link href="/">
       <Image
-        src="/companies/IT_Perfect_Logo.png"
+        src={LogoImage}
         width={200}
         height={100}
         alt="IT Perfect Logo"
         className={` cursor-pointer w-24 h-24 dark:invert ${className}`}
-        unoptimized
       />
     </Link>
   );
